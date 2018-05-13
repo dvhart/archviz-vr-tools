4c5fc534fdd2b2370e2b776953e1d9143fabc346
Darren Hart <dvhart@infradead.org>
2018-05-12 18:48:55 -0700

    Initial commit
    Python script to create Google Cardboard viewable stereoscopic
    photospheres as defined by:
    
    https://developers.google.com/vr/reference/cardboard-camera-vr-photo-format
    
    Depends on pillow, python-xmp-toolkit and exempi.
    
    Signed-off-by: Darren Hart <dvhart@infradead.org>

---
+++ README.md
@@ -0,0 +1,94 @@
+# archviz-vr-tools
+Collection of tools for architectural visualization virtual reality workflow optimization.
+
+# mkvrjpg.py
+Some photorealistic rendering tools with create stere output, but they often come in simple image form which require some post processing to view them as photospheres or as stereoscopic photospheres in a VR headset.
+
+mkvrjpg.py assembles a stereoscopic jpeg image from left and right eye jpeg images.  These images can be separate files, or a single image with both images, either stacked (left on top) or side by side (left on the left). It sets the required XMP properties and embeds the two images which allows the image to be viewed in any image viewer (left eye image), as well as in photosphere viewers such as the Google Photos web viewer, and in stereoscopic VR viewers, such as Google Carboard and Google Daydream.
+
+## Installation
+### Mac OS
+You will first need to install python 3 and homebrew, see the respective links
+and follow the instructions to install before proceeding.
+
+From a terminal, install the dependencies:
+```
+$ brew install exempi
+$ pip3 install pillow python-xmp-toolkit
+```
+
+Copy the mkvrjpg.py script to your PATH, such as /usr/local/bin.
+`$ sudo cp mkvrjpg.py /usr/local/bin/`
+
+### Linux
+Some packages are likely available from your distribution, such as python3, python3-pillow, and exempi.
+
+You will most likely need to use pip to install python-xmp-toolkit.
+
+#### Fedora
+`$ sudo dnf install python3 python3-pillow exempi`
+
+#### PIP
+`$ pip3 install python-xmp-toolkit`
+
+## Usage
+Use the `-h` (or `--help`) option to display help on basic usage.
+```
+$ ./mkvrjpg.py -h
+Usage: mkvrjpg.py [OPTIONS] [stereo_img || left_img right_img]
+  -h, --help          display this help and exit
+  -l, --left          left eye image
+  -o, --out           output image
+  -r, --right         right eye image
+  -s, --stereo        stereo image
+```
+### Input
+Photospheres should be made up of two images with a 2:1 aspect ratio. Stereo
+images cat be stacked (left on top) in a 1:1 aspect ratio, or side by side (left
+on the left) in a 4:1 aspect ratio. Stereo images will be split into two
+temporary left and right eye images with a 2:1 aspect ratio.
+
+### Examples
+mkvrjpg.py will operate on either left and right eye image pairs or on single
+stereo images. It will accept positional arguments, or allow you to use the
+documented options to explicitly specify each input image.
+
+#### Create a VR image from a stereo image
+If your input image is a single stereo image in either stacked or side by side
+format, mkvrjpg.py will split it into the required left and right eye images
+automatically.
+
+```
+$ mkvrjpg.py stereo.jpg
+VR Image saved to: /Users/dvhart/source/archviz-vr-tools/mkvrjpg-fkwg9wt_.vr.jpg
+```
+This is equivalent to:
+```
+$ mkvrjpg.py -s stereo.jpg
+VR Image saved to: /Users/dvhart/source/archviz-vr-tools/mkvrjpg-peblq_9e.vr.jpg
+```
+#### Create a VR image from a left and right eye image pair
+```
+$ mkvrjpg.py left.jpg right.jpg
+VR Image saved to: /Users/dvhart/source/archviz-vr-tools/mkvrjpg-vo051rxk.vr.jpg
+```
+This is equivalent to:
+```
+$ mkvrjpg.py -l left.jpg -r right.jpg
+VR Image saved to: /Users/dvhart/source/archviz-vr-tools/mkvrjpg-irgm8_98.vr.jpg
+```
+
+#### Specify the output image
+You can specify the output filename with the `-o` option. Note that it must come
+before any position arguments, but can appear in any order relative to other
+options. A `.vr.jpg` suffix is recommended for viewer compatibility.
+
+```
+$ mkvrjpg.py -o output.vr.jpg stereo.jpg
+VR Image saved to: output.vr.jpg
+```
+This is equivalent to:
+```
+$ mkvrjpg.py -s stereo.jpg -o output.vr.jpg
+VR Image saved to: output.vr.jpg
+```
---
+++ mkvrjpg.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+# Copyright (c) 2018-2019, Darren Hart and the archviz-vr-tools contributors
+# SPDX-License-Identifier: BSD 2-Clause "Simplified" License
+#
+# mkvrjpg.py assembles a stereoscopic jpeg image from left and right eye
+# images.  These images can be separate files, or a single image with
+# both images, either stacked (left on top) or side by side (left on the
+# left). It sets the required XMP properties and embeds the two images
+# which allows the image to be viewed in any image viewer (left eye
+# image), as well as in photosphere viewers such as the Google Photos
+# web viewer, and in stereoscopic VR viewers, such as Google Carboard
+# [1] and Google Daydream.
+#
+# Thanks to Andrew Perry and the Cardboard Camera Toolkit project for
+# for the functional example of how to embed the GImage properties.
+# https://bitbucket.org/pansapiens/cardboardcam/src/master/
+#
+# 1. https://developers.google.com/vr/reference/cardboard-camera-vr-photo-format
+
+# Python built-in dependencies
+import atexit
+import base64
+import getopt
+import imghdr
+import os
+import sys
+from shutil import copyfile
+from tempfile import mkstemp
+
+# External dependencies
+import libxmp
+from libxmp import XMPFiles
+from libxmp.consts import XMP_NS_TIFF
+from PIL import Image
+
+# XMP property namespaces
+GPANO = u'http://ns.google.com/photos/1.0/panorama/'
+GIMAGE = u'http://ns.google.com/photos/1.0/image/'
+GAUDIO = u'http://ns.google.com/photos/1.0/audio/'
+
+
+def usage():
+    print('Usage: %s [OPTIONS] [stereo_img || left_img right_img]' %
+            os.path.basename(sys.argv[0]))
+    print('  -h, --help          display this help and exit')
+    print('  -l, --left          left eye image')
+    print('  -o, --out           output image')
+    print('  -r, --right         right eye image')
+    print('  -s, --stereo        stereo image')
+
+
+# Verify the image exists and is a jpeg image
+def check_image(image):
+    if not os.path.isfile(image):
+        print("%s does not exist or is not a file" % image)
+        return False
+    if not imghdr.what(image) == 'jpeg':
+        print("%s is not a jpeg image" % image)
+        return False
+    return True
+
+
+# Embed 'image' as an xmp data property, this is how we have both the
+# left and the right eye contained in the same image, while still being
+# able to view it in normal viewers as a single image.
+def add_gimage_xmp(xmp, image):
+    f = open(image, 'rb')
+    data = f.read()
+    f.close()
+    b64data = base64.b64encode(data)
+    xmp.set_property(GIMAGE, u'GImage:Mime', 'image/jpeg')
+    xmp.set_property(GIMAGE, u'GImage:Data', b64data.decode('utf-8'))
+    del b64data
+
+
+# Add the XMP properties and embed the left and right image data in the
+# image specified by out.
+def make_vr_image(left, right, out):
+    out_image = Image.open(out)
+    width, height = out_image.size
+    out_image.close()
+
+    xmpfile = XMPFiles(file_path=out, open_forupdate=True)
+    xmp = xmpfile.get_xmp()
+
+    xmp.register_namespace(GPANO, 'GPano')
+    xmp.register_namespace(GIMAGE, 'GImage')
+    xmp.register_namespace(GAUDIO, 'GAudio')
+    xmp.register_namespace(XMP_NS_TIFF, 'tiff')
+
+    xmp.set_property_int(GPANO, u'GPano:CroppedAreaLeftPixels', 0)
+    xmp.set_property_int(GPANO, u'GPano:CroppedAreaTopPixels', 0)
+    xmp.set_property_int(GPANO, u'GPano:CroppedAreaImageWidthPixels', width)
+    xmp.set_property_int(GPANO, u'GPano:CroppedAreaImageHeightPixels', height)
+    xmp.set_property_int(GPANO, u'GPano:FullPanoWidthPixels', width)
+    xmp.set_property_int(GPANO, u'GPano:FullPanoHeightPixels', int(width/2.0))
+    xmp.set_property_float(GPANO, u'GPano:InitialViewHeadingDegrees', 180)
+
+    xmp.set_property_int(XMP_NS_TIFF, u'tiff:ImageWidth', width)
+    xmp.set_property_int(XMP_NS_TIFF, u'tiff:ImageHeight', height)
+    xmp.set_property_int(XMP_NS_TIFF, u'tiff:Orientation', 0)
+    xmp.set_property(XMP_NS_TIFF, u'tiff:Make', 'mkvrjpg')
+    xmp.set_property(XMP_NS_TIFF, u'tiff:Model', '')
+
+    add_gimage_xmp(xmp, left)
+    add_gimage_xmp(xmp, right)
+
+    if xmpfile.can_put_xmp(xmp):
+        xmpfile.put_xmp(xmp)
+        print("VR Image saved to: %s" % out)
+    else:
+        print("Error: cannot put xmp:")
+        print(xmp)
+    xmpfile.close_file()
+
+# Split a stereo image into temporary left and right eye images
+def split_stereo(stereo, left, right):
+        stereo_img = Image.open(stereo)
+        width, height = stereo_img.size
+        if (width == height):
+            # left on top, right on bottom
+            left_img = stereo_img.crop((0, 0, width, int(height/2.0)))
+            right_img = stereo_img.crop((0, int(height/2.0), width, height))
+        elif (width == 4 * height):
+            # left then right
+            left_img = stereo_img.crop((0, 0, int(width/2.0), height))
+            right_img = stereo_img.crop((int(width/2.0), 0, width, height))
+        else:
+            print("Unknown stereo image layout: %s x %s", width, height)
+            stereo_img.close()
+            exit(1)
+
+        left_img.save(left)
+        right_img.save(right)
+        stereo_img.close()
+        atexit.register(os.remove, left)
+        atexit.register(os.remove, right)
+
+
+def main():
+    left = None
+    right = None
+    out = None
+    stereo = None
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hl:r:o:s:",
+                ["help","left=","right=","out=","stereo="])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            sys.exit(0)
+        elif o in ('-s', '--stereo'):
+            if (left or right):
+                usage()
+                exit(1)
+            stereo = a
+        elif o in ('-l', '--left'):
+            if (stereo):
+                usage()
+                exit(1)
+            left = a
+        elif o in ('-r', '--right'):
+            if (stereo):
+                usage()
+                exit(1)
+            right = a
+        elif o in ('-o', '--out'):
+            out = a
+            if os.path.exists(out):
+                print("Output file already exists: %s" % out)
+                exit(1)
+        else:
+            assert False, "unhandled option"
+
+    # Verify we received either stereo or both the left and the right image
+    # Verify the arguments are consistent
+    if not (stereo or (left and right)):
+        if len(args) == 1:
+            stereo = args[0]
+        elif len(args) == 2:
+            left = args[0]
+            right = args[1]
+        else:
+            print('Invalid number of arguments: %s' % len(args))
+            usage()
+            exit(1)
+    elif len(args) > 0:
+        usage()
+        exit(1)
+
+    if (stereo):
+        if not check_image(stereo):
+            exit(1)
+
+        # Create the temporary left and right images
+        fd, left = mkstemp("-left.jpg")
+        os.close(fd)
+        fd, right = mkstemp("-right.jpg")
+        os.close(fd)
+
+        split_stereo(stereo, left, right)
+
+    if not check_image(left) or not check_image(right):
+        exit(1)
+
+    # Create an output name if one is not provided
+    if not out:
+        fd, out = mkstemp('.vr.jpg', 'mkvrjpg-', os.getcwd())
+        os.close(fd)
+
+    # Create an output image to start with
+    copyfile(left, out)
+
+    # Image files are ready go, create the vr image
+    make_vr_image(left, right, out)
+
+if __name__ == "__main__":
+    main()
